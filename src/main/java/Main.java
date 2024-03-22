6c3d7a648cc0c4203cc470b6f6dab42ec38712b0
Alyona Redina <medvedeva_64@mail.ru>
2024-03-22 22:01:06 +0600

    corrected the field in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        public static void birthday (String[]Post post){
-            post.birthday = new FormDate();
-
-            post.birthday.day = 13;
-            post.birthday.month = 6;
-            post.birthday.year = 1999;
-        }
-
         Post post = new Post();
 
         post.name = "Иван";
@@ -18,6 +10,13 @@ public class Main {
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
         post.subscription = true;
+
+
+        post.birthday = new FormDate();
+
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
 
     }
 }
